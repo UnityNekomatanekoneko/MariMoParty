49e1bb6581359c183ac4136e9578384fada9c12d
Gussan <kbc15b02@stu.kawahara.ac.jp>
2016-11-11 11:30:34 +0900

    change

---
+++ Assets/Resources/Scripts/Common/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public static void Change(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
